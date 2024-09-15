b739b86b663bc128dee3f1faae6ec9a2733ac731
Abhijit808 <abhijitrayarao@gmail.com>
2024-09-15 23:50:50 +0530

    tp001

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const gridItems = document.querySelectorAll('.grid-item');
-
-    gridItems.forEach(item => {
-        item.addEventListener('click', () => {
-            const details = item.querySelector('.details');
-            const isVisible = details.style.display === 'block';
-            
-            // Hide all details
-            document.querySelectorAll('.details').forEach(detail => {
-                detail.style.display = 'none';
-            });
-
-            // Toggle current details
-            details.style.display = isVisible ? 'none' : 'block';
-        });
-    });
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const line2 = document.querySelector('.line2')
+const line1 = document.querySelector('.line1')
+const line3 = document.querySelector('.line3')
+const click = document .querySelectorAll('.click')
+const body = document.querySelector('body')
+const form = document.querySelector('form')
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    body.classList.toggle('overflowY_hidden')
+    line2.classList.toggle('line_2_rotate')
+    line3.classList.toggle('line_3_rotate')
+    line1.classList.toggle('remove')
 });
+
+click.forEach((click)=>{
+    click.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+        body.classList.toggle('overflowY_hidden')
+        line2.classList.toggle('line_2_rotate')
+        line3.classList.toggle('line_3_rotate')
+        line1.classList.toggle('remove')
+    })
+    
+   
+})
+form.addEventListener('click',()=>{
+    alert("Form submitted")
+})
