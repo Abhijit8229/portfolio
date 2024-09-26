f7940533a9cf5a1c5d103eb9958073adcfd53762
Abhijit808 <abhijitrayarao@gmail.com>
2024-09-26 18:22:38 +0530

    changes90902

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const line3 = document.querySelector('.line3')
 const click = document .querySelectorAll('.click')
 const body = document.querySelector('body')
 const form = document.querySelector('form')
+const active = document.querySelectorAll('.clicked')
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
     body.classList.toggle('overflowY_hidden')
@@ -13,11 +14,16 @@ burger.addEventListener('click', () => {
     line3.classList.toggle('line_3_rotate')
     line1.classList.toggle('remove')
 });
-
+click.forEach(it=>{
+    it.addEventListener('click',(e)=>{
+        click.forEach(it=>it.classList.remove('act'))
+        e.target.classList = 'act'
+    })
+})
 click.forEach((click)=>{
     click.addEventListener('click',()=>{
         nav.classList.toggle('nav-active');
-        body.classList.toggle('overflowY_hidden')
+        // body.classList.toggle('overflowY_hidden')
         line2.classList.toggle('line_2_rotate')
         line3.classList.toggle('line_3_rotate')
         line1.classList.toggle('remove')
@@ -29,4 +35,12 @@ form.addEventListener('submit',(e)=>{
     e.preventDefault()
     alert("Response Recived Thank you")
     form.reset()
+})
+
+
+active.forEach(act=>{
+    act.addEventListener('click',(e)=>{
+        active.forEach((it=>it.classList.remove("act")))
+        act.classList.add("act")
+    })
 })
